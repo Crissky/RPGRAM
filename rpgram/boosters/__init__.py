fed86d8f40d0fa21a46d41b5440931916d86bf09
Edson Cristovam <pg2006pe@hotmail.com>
2023-04-26 22:44:25 -0300

    Adiciona a classe Classe

--- rpgram/boosters/__init__.py
+++ rpgram/boosters/__init__.py
@@ -1,2 +1,3 @@
 from rpgram.boosters.stats_booster import StatsBooster
 from rpgram.boosters.race import Race
+from rpgram.boosters.classe import Classe
---
+++ rpgram/boosters/classe.py
@@ -0,0 +1,72 @@
+from rpgram.boosters import StatsBooster
+
+
+class Classe(StatsBooster):
+    def __init__(
+        self,
+        name: str,
+        description: str = '',
+        bonus_strength: int = 0,
+        bonus_dexterity: int = 0,
+        bonus_constitution: int = 0,
+        bonus_intelligence: int = 0,
+        bonus_wisdom: int = 0,
+        bonus_charisma: int = 0,
+        multiplier_strength: float = 1.0,
+        multiplier_dexterity: float = 1.0,
+        multiplier_constitution: float = 1.0,
+        multiplier_intelligence: float = 1.0,
+        multiplier_wisdom: float = 1.0,
+        multiplier_charisma: float = 1.0,
+    ) -> None:
+        super().__init__(
+            bonus_strength=bonus_strength,
+            bonus_dexterity=bonus_dexterity,
+            bonus_constitution=bonus_constitution,
+            bonus_intelligence=bonus_intelligence,
+            bonus_wisdom=bonus_wisdom,
+            bonus_charisma=bonus_charisma,
+            multiplier_strength=multiplier_strength,
+            multiplier_dexterity=multiplier_dexterity,
+            multiplier_constitution=multiplier_constitution,
+            multiplier_intelligence=multiplier_intelligence,
+            multiplier_wisdom=multiplier_wisdom,
+            multiplier_charisma=multiplier_charisma,
+        )
+        self.__name = name
+        self.__description = description
+
+    def get_sheet(self) -> str:
+        return (
+            f'Classe: {self.name}\n'
+            f'Descrição: {self.description}\n'
+        )
+
+    def __repr__(self) -> str:
+        return (
+            f'########################################\n'
+            f'{self.get_sheet()}'
+            f'{super().get_sheet()}'
+            f'########################################\n'
+        )
+
+    name = property(lambda self: self.__name)
+    description = property(lambda self: self.__description)
+
+if __name__ == '__main__':
+    classe = Classe(
+        'Clérigo', 'Clérigo de teste',
+        bonus_strength=5,
+        bonus_dexterity=5,
+        bonus_constitution=8,
+        bonus_intelligence=10,
+        bonus_wisdom=16,
+        bonus_charisma=16,
+        multiplier_strength=0.5,
+        multiplier_dexterity=0.5,
+        multiplier_constitution=1.0,
+        multiplier_intelligence=1.0,
+        multiplier_wisdom=1.8,
+        multiplier_charisma=1.7,
+    )
+    print(classe)
