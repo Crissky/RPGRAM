c0255e277dacf3e37d1456e4f087591551c17a38
Edson Cristovam <pg2006pe@hotmail.com>
2023-04-27 22:17:14 -0300

    Adiciona Classe ao personagem base

--- rpgram/characters/char_base.py
+++ rpgram/characters/char_base.py
@@ -1,4 +1,4 @@
-from rpgram.boosters import Race
+from rpgram.boosters import Race, Classe
 from rpgram.stats import BaseStats, CombatStats
 
 
@@ -8,6 +8,7 @@ class BaseCharacter:
         char_name: str,
         base_stats: BaseStats = None,
         combat_stats: CombatStats = None,
+        classe: Classe = None,
         race: Race = None,
         level: int = 1,
         base_strength: int = 0,
@@ -30,6 +31,20 @@ class BaseCharacter:
         race_multiplier_intelligence: float = 1.0,
         race_multiplier_wisdom: float = 1.0,
         race_multiplier_charisma: float = 1.0,
+        classe_name: str = '',
+        classe_description: str = '',
+        classe_bonus_strength: int = 0,
+        classe_bonus_dexterity: int = 0,
+        classe_bonus_constitution: int = 0,
+        classe_bonus_intelligence: int = 0,
+        classe_bonus_wisdom: int = 0,
+        classe_bonus_charisma: int = 0,
+        classe_multiplier_strength: float = 1.0,
+        classe_multiplier_dexterity: float = 1.0,
+        classe_multiplier_constitution: float = 1.0,
+        classe_multiplier_intelligence: float = 1.0,
+        classe_multiplier_wisdom: float = 1.0,
+        classe_multiplier_charisma: float = 1.0,
     ) -> None:
         self.__name = char_name
         if not isinstance(race, Race):
@@ -50,6 +65,24 @@ class BaseCharacter:
                 multiplier_charisma=race_multiplier_charisma,
             )
         self.__race = race
+        if not isinstance(classe, Classe):
+            classe = Classe(
+                name=classe_name,
+                description=classe_description,
+                bonus_strength=classe_bonus_strength,
+                bonus_dexterity=classe_bonus_dexterity,
+                bonus_constitution=classe_bonus_constitution,
+                bonus_intelligence=classe_bonus_intelligence,
+                bonus_wisdom=classe_bonus_wisdom,
+                bonus_charisma=classe_bonus_charisma,
+                multiplier_strength=classe_multiplier_strength,
+                multiplier_dexterity=classe_multiplier_dexterity,
+                multiplier_constitution=classe_multiplier_constitution,
+                multiplier_intelligence=classe_multiplier_intelligence,
+                multiplier_wisdom=classe_multiplier_wisdom,
+                multiplier_charisma=classe_multiplier_charisma,
+            )
+        self.__classe = classe
         if not isinstance(base_stats, BaseStats):
             base_stats = BaseStats(
                 level,
@@ -60,6 +93,7 @@ class BaseCharacter:
                 base_wisdom,
                 base_charisma,
                 self.__race,
+                self.__classe,
             )
         self.__base_stats = base_stats
         if not isinstance(combat_stats, CombatStats):
@@ -73,12 +107,14 @@ class BaseCharacter:
     base_stats = bs = property(fget=lambda self: self.__base_stats)
     combat_stats = cs = property(fget=lambda self: self.__combat_stats)
     race = property(fget=lambda self: self.__race)
+    classe = property(fget=lambda self: self.__classe)
 
     def __repr__(self) -> str:
         return (
             f'########################################\n'
             f'Nome: {self.name}\n'
             f'{self.race.get_sheet()}'
+            f'{self.classe.get_sheet()}'
             f'{self.base_stats.get_sheet()}'
             f'{self.combat_stats.get_sheet()}'
             f'########################################\n'
@@ -104,11 +140,25 @@ if __name__ == '__main__':
         race_bonus_wisdom=12,
         race_bonus_charisma=10,
         race_multiplier_strength=1.0,
-        race_multiplier_dexterity=1.2,
-        race_multiplier_constitution=1,
-        race_multiplier_intelligence=1.0,
+        race_multiplier_dexterity=1.0,
+        race_multiplier_constitution=1.0,
+        race_multiplier_intelligence=1.2,
         race_multiplier_wisdom=1.2,
         race_multiplier_charisma=1.0,
+        classe_name='Arqueiro',
+        classe_description='Arqueiro Teste',
+        classe_bonus_strength=5,
+        classe_bonus_dexterity=15,
+        classe_bonus_constitution=10,
+        classe_bonus_intelligence=10,
+        classe_bonus_wisdom=10,
+        classe_bonus_charisma=10,
+        classe_multiplier_strength=1,
+        classe_multiplier_dexterity=1.5,
+        classe_multiplier_constitution=1,
+        classe_multiplier_intelligence=1,
+        classe_multiplier_wisdom=1,
+        classe_multiplier_charisma=1,
     )
     print(base_character)
     base_character.base_stats.xp = 100
