05aa92a7e1e4f61e544f600b7e0b703fe0b32dde
Edson Cristovam <pg2006pe@hotmail.com>
2023-04-29 00:39:09 -0300

    Adiciona Import Equipament

--- rpgram/boosters/__init__.py
+++ rpgram/boosters/__init__.py
@@ -1,3 +1,4 @@
 from rpgram.boosters.stats_booster import StatsBooster
 from rpgram.boosters.race import Race
 from rpgram.boosters.classe import Classe
+from rpgram.boosters.equipament import Equipament
