e9f30400da06c61177fa574fad224b37b992e47c
Edson Cristovam <pg2006pe@hotmail.com>
2023-05-01 19:22:21 -0300

    Adiciona Models imports

--- repository/mongo/__init__.py
+++ repository/mongo/__init__.py
@@ -2,3 +2,5 @@ from repository.mongo.collection_enum import CollectionEnum
 from repository.mongo.database import Database
 from repository.mongo.models.model import Model
 from repository.mongo.models.player import PlayerModel
+from repository.mongo.models.char_base import BaseCharacterModel
+from repository.mongo.models.char_player import PlayerCharacterModel
