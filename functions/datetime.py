f6b3eeec079f99646a8cd365322b5e8a2dd53c09
Edson Cristovam <pg2006pe@hotmail.com>
2023-05-06 01:38:03 -0300

    Adiciona função datetime_to_string

--- functions/datetime.py
+++ functions/datetime.py
@@ -5,3 +5,9 @@ def get_brazil_time_now():
     delta = timedelta(hours=3)
     dt = datetime.utcnow() - delta
     return dt
+
+
+def datetime_to_string(dt: datetime) -> str:
+    if isinstance(dt, datetime):
+        dt = dt.strftime("%d/%m/%Y %H:%M:%S")
+    return dt
