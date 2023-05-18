efa069fcd40b9c3869db78bea773807a29a98e90
Edson Cristovam <pg2006pe@hotmail.com>
2023-05-17 23:05:32 -0300

    Corrige decorators imports

--- bot/decorators/__init__.py
+++ bot/decorators/__init__.py
@@ -1,3 +1,3 @@
+from bot.decorators.print import print_basic_infos
 from bot.decorators.char import need_have_char
 from bot.decorators.char import skip_if_no_have_char
-from bot.decorators.print import print_basic_infos
