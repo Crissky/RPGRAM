5a0b82b6b3a1cfc8fe78fe71ab534ddad57d27d3
Edson Cristovam <pg2006pe@hotmail.com>
2023-06-10 02:24:34 -0300

    Adiciona BattleNotEndError

--- rpgram/errors.py
+++ rpgram/errors.py
@@ -1,2 +1,5 @@
 class CurrentPlayerTurnError(Exception):
     ''' Erro quando não é o turno do personagem.'''
+
+class BattleNotEndError(Exception):
+    ''' Erro quando a batalha ainda não acabou.'''
