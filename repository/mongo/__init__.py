11e8651322c39f25f12dc9720042564af52e5ed3
Edson Cristovam <pg2006pe@hotmail.com>
2023-07-30 19:57:46 -0300

    Adiciona import para Bagmodel

--- repository/mongo/__init__.py
+++ repository/mongo/__init__.py
@@ -9,3 +9,4 @@ from repository.mongo.models.player import PlayerModel
 from repository.mongo.models.race import RaceModel
 from repository.mongo.models.battle import BattleModel
 from repository.mongo.models.equips import EquipsModel
+from repository.mongo.models.bag import BagModel
