f7e3b0990c4eb67100ec26094f8cfccdedb79ef0
Edson Cristovam <pg2006pe@hotmail.com>
2023-10-08 17:53:40 -0300

    Atualiza itens de cura para usar cure_hit_points

--- repository/mongo/populate/consumable.py
+++ repository/mongo/populate/consumable.py
@@ -8,56 +8,65 @@ CONSUMABLES = [
         'name': 'Minor Healing Potion',
         'description': 'Cura 100 de HP.',
         'weight': 0.1,
-        'function': 'target.combat_stats.hp = 100',
+        'function': 'return target.combat_stats.cure_hit_points(100)',
         'rarity': RarityEnum.COMMON.name,
     }, {
         'name': 'Light Healing Potion',
         'description': 'Cura 250 de HP.',
         'weight': 0.1,
-        'function': 'target.combat_stats.hp = 250',
+        'function': 'return target.combat_stats.cure_hit_points(250)',
         'rarity': RarityEnum.COMMON.name,
     }, {
         'name': 'Healing Potion',
         'description': 'Cura 500 de HP.',
         'weight': 0.1,
-        'function': 'target.combat_stats.hp = 500',
+        'function': 'return target.combat_stats.cure_hit_points(500)',
         'rarity': RarityEnum.COMMON.name,
     }, {
         'name': 'Greater Healing Potion',
         'description': 'Cura 1000 de HP.',
         'weight': 0.1,
-        'function': 'target.combat_stats.hp = 1000',
+        'function': 'return target.combat_stats.cure_hit_points(1000)',
         'rarity': RarityEnum.UNCOMMON.name,
     }, {
         'name': 'Rare Healing Potion',
         'description': 'Cura 2000 de HP.',
         'weight': 0.1,
-        'function': 'target.combat_stats.hp = 2000',
+        'function': 'return target.combat_stats.cure_hit_points(2000)',
         'rarity': RarityEnum.RARE.name,
     }, {
         'name': 'Epic Healing Potion',
         'description': 'Cura 5000 de HP.',
         'weight': 0.1,
-        'function': 'target.combat_stats.hp = 5000',
+        'function': 'return target.combat_stats.cure_hit_points(5000)',
         'rarity': RarityEnum.EPIC.name,
     }, {
         'name': 'Legendary Healing Potion',
         'description': 'Cura 10000 de HP.',
         'weight': 0.1,
-        'function': 'target.combat_stats.hp = 10000',
+        'function': 'return target.combat_stats.cure_hit_points(10000)',
         'rarity': RarityEnum.LEGENDARY.name,
     }, {
         'name': 'Mythic Healing Potion',
         'description': 'Cura TODO de HP.',
         'weight': 0.1,
-        'function': 'target.combat_stats.hp = target.combat_stats.hp',
+        'function': 'return target.combat_stats.cure_hit_points(target.combat_stats.hp)',
         'rarity': RarityEnum.MYTHIC.name,
     },
 ]
 
 if __name__ == "__main__":
     items_model = ItemModel()
+    fields = ['_id', 'name', 'created_at']
     for consumable_dict in CONSUMABLES:
+        consumable_name = consumable_dict['name']
+        mongo_dict = items_model.get(
+            query={'name': consumable_name},
+            fields=fields,
+        )
+        if mongo_dict:
+            for field in fields:
+                consumable_dict[field] = mongo_dict[field]
         consumable = Consumable(**consumable_dict)
         print(consumable)
         items_model.save(consumable)
