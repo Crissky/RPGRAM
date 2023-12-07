62e0557d128bc6a4e28fab7846948552d9646970
Edson Cristovam <pg2006pe@hotmail.com>
2023-12-07 13:00:43 -0300

    WIP: Adicionando create_random_enemy

--- repository/mongo/populate/enemy.py
+++ repository/mongo/populate/enemy.py
@@ -1,11 +1,55 @@
+from repository.mongo.populate.tools import random_group_level, weighted_choice
 from rpgram.characters import NPCharacter
+from rpgram.enums import EnemyStarsEnum
 
 
-def create_random_enemy(group_level: int) -> NPCharacter:
-    '''Cria inimigos de maneira aleatória.'''
-    max_number_enemies = 5
+def get_total_enemy(max_number_enemies: int = 5):
+    '''Retorna o número total de inimigos que serão criados'''
     number_enemies_probs = {
-        i+1: j+1
-        for i, j in enumerate(range(max_number_enemies, -1, -1))
+        f'{i+1}': j
+        for i, j in enumerate(range(max_number_enemies, 0, -1))
     }
-    
+    return weighted_choice(**number_enemies_probs)
+
+
+def choice_enemy_star(no_boss: bool = False) -> str:
+    '''Função que retorna um tipo de item aleatório.
+    O tipo do item é retornado com base em sua propabilidade.
+    '''
+    types_item = {
+        EnemyStarsEnum.ONE.name: 1000,
+        EnemyStarsEnum.TWO.name: 900,
+        EnemyStarsEnum.THREE.name: 800,
+        EnemyStarsEnum.FOUR.name: 700,
+        EnemyStarsEnum.FIVE.name: 600,
+        EnemyStarsEnum.BOSS.name: 100,
+    }
+
+    if no_boss:
+        types_item.pop('TRAP')
+
+    return weighted_choice(**types_item)
+
+
+def create_random_enemy(
+    group_level: int,
+    no_boss: bool = False
+) -> NPCharacter:
+    '''Cria inimigos de maneira aleatória.'''
+    total_enemy = get_total_enemy()
+    for _ in range(total_enemy):
+        enemy_level = random_group_level(group_level)
+        enemy_stars = choice_enemy_star(no_boss=no_boss)
+        if EnemyStarsEnum.BOSS.name == enemy_stars:
+            no_boss = True
+
+
+if __name__ == '__main__':
+    from collections import Counter
+    items = []
+    for i in range(1000):
+        items.append(choice_enemy_star())
+    result = Counter(items)
+    for item in result.most_common():
+        print(f'{item[0]}: {item[1]},', end=' ')
+    print()
