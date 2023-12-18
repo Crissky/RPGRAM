1fe0512f05e56dc8b95265de77ff8fb1ef711b38
Edson Cristovam <pg2006pe@hotmail.com>
2023-12-18 19:31:34 -0300

    Adiciona atributo Alinhamento em NPCharacter

--- rpgram/characters/char_non_player.py
+++ rpgram/characters/char_non_player.py
@@ -1,9 +1,13 @@
 from datetime import datetime
+from typing import Union
 from bson import ObjectId
+from function.text import escape_basic_markdown_v2, remove_bold, remove_code
 from rpgram.boosters.classe import Classe
 from rpgram.boosters.race import Race
 from rpgram.characters.char_base import BaseCharacter
-from rpgram.enums.enemy import EnemyStarsEnum
+from rpgram.constants.text import ALIGNMENT_EMOJI_TEXT
+from rpgram.enums.emojis import EmojiEnum
+from rpgram.enums.enemy import AlignmentEnum, EnemyStarsEnum
 from rpgram.equips import Equips
 from rpgram.status import Status
 
@@ -24,6 +28,7 @@ class NPCharacter(BaseCharacter):
         char_name: str,
         classe: Classe,
         race: Race,
+        alignment: Union[AlignmentEnum, str],
         enemy_id: ObjectId = None,
         equips: Equips = None,
         status: Status = None,
@@ -35,13 +40,28 @@ class NPCharacter(BaseCharacter):
         base_intelligence: int = 0,
         base_wisdom: int = 0,
         base_charisma: int = 0,
-        stars: str = EnemyStarsEnum.THREE.name,
+        stars: Union[EnemyStarsEnum, str] = EnemyStarsEnum.THREE,
         combat_damage: int = 0,
         _id: ObjectId = None,
         created_at: datetime = None,
         updated_at: datetime = None
     ) -> None:
-        points_multiplier = POINTS_MULTIPLIER[stars]
+        if isinstance(alignment, str):
+            alignment = AlignmentEnum[alignment]
+        elif not isinstance(alignment, AlignmentEnum):
+            raise ValueError(
+                f'alignment precisa ser uma string ou AlignmentEnum.'
+            )
+
+        if isinstance(stars, str):
+            stars = EnemyStarsEnum[stars]
+        elif not isinstance(stars, EnemyStarsEnum):
+            raise ValueError(
+                f'stars precisa ser uma string ou EnemyStarsEnum.'
+            )
+
+        points_multiplier = POINTS_MULTIPLIER[stars.name]
+
         super().__init__(
             char_name=char_name,
             classe=classe,
@@ -63,6 +83,8 @@ class NPCharacter(BaseCharacter):
             created_at=created_at,
             updated_at=updated_at
         )
+
+        self.__alignment = alignment
         self.__stars = stars
 
     # Getters
@@ -71,13 +93,43 @@ class NPCharacter(BaseCharacter):
         return super().name + f'({self.emoji_stars})'
 
     @property
-    def stars(self) -> int:
+    def alignment(self) -> AlignmentEnum:
+        return self.__alignment
+
+    @property
+    def alignment_name(self) -> str:
+        return self.__alignment.value
+
+    @property
+    def stars(self) -> EnemyStarsEnum:
         return self.__stars
 
     @property
     def emoji_stars(self):
-        return EnemyStarsEnum[self.stars].value
+        return self.stars.value
 
     @property
     def enemy_id(self) -> ObjectId:
         return super().player_id
+
+    def get_sheet(self, verbose: bool = False, markdown: bool = False) -> str:
+        text = f'*{ALIGNMENT_EMOJI_TEXT}*: {self.alignment_name}\n'
+
+        if not markdown:
+            text = remove_bold(text)
+            text = remove_code(text)
+        else:
+            text = escape_basic_markdown_v2(text)
+
+        text += f'{super().get_sheet(verbose, markdown)}'
+
+        return text
+
+    def to_dict(self):
+        _dict = {
+            'alignment': self.alignment.name,
+            'stars': self.stars.name
+        }
+        _dict.update(super().to_dict())
+
+        return _dict
