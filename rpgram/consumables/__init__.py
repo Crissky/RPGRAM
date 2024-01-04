e8ad21e4b341cbd0f2a712213a53608c06a2ac95
Edson Cristovam <pg2006pe@hotmail.com>
2024-01-04 17:08:50 -0300

    Adiciona item de reviver e atualiza função de reviver

--- rpgram/consumables/__init__.py
+++ rpgram/consumables/__init__.py
@@ -1,5 +1,6 @@
 from rpgram.consumables.consumable import Consumable
 from rpgram.consumables.heal import HealingConsumable
+from rpgram.consumables.heal import ReviveConsumable
 from rpgram.consumables.cure import CureConsumable
 from rpgram.consumables.other import IdentifyingConsumable
 from rpgram.consumables.other import XPConsumable
