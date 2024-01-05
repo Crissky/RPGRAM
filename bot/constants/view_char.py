d887d2882e96baf95978599ffdb38fe290fb8d5c
Edson Cristovam <pg2006pe@hotmail.com>
2024-01-05 20:06:41 -0300

    Adiciona botão de refresh

--- bot/constants/view_char.py
+++ bot/constants/view_char.py
@@ -3,3 +3,11 @@ COMMANDS = ['personagem', 'char']
 
 # TEXTS
 SECTION_TEXT_CHAR = 'PERSONAGEM'
+REFRESH_VIEW_CHAR_PATTERN = 'refresh_view_char'
+
+
+# ALERT BUTTON TEXTS
+ACCESS_DENIED = (
+    f'⛔VOCÊ NÃO TEM ACESSO A ESSE PERSONAGEM⛔\n\n'
+    f'Use o comando !{COMMANDS[0]} para acessar o SEU PERSONAGEM.'
+)
