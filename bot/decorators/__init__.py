668524332191c43e6100e6eb2e93c032cec8f602
Edson Cristovam <pg2006pe@hotmail.com>
2024-03-02 00:47:29 -0300

    Atualiza exibição de Classe e Raça Agora passa a usar a função de converte o callback_data.

--- bot/decorators/__init__.py
+++ bot/decorators/__init__.py
@@ -9,6 +9,9 @@ from bot.decorators.job import skip_if_spawn_timeout
 from bot.decorators.player import need_singup_player
 from bot.decorators.player import skip_if_no_singup_player
 from bot.decorators.player import alert_if_not_chat_owner
+from bot.decorators.player import (
+    alert_if_not_chat_owner_to_callback_data_to_dict
+)
 from bot.decorators.print import print_basic_infos
 from bot.decorators.battle import need_not_in_battle
 from bot.decorators.retry import retry_after
