ba3f897c65add022164f83391d8cc398d3c8ce7e
Edson Cristovam <pg2006pe@hotmail.com>
2024-06-04 13:41:46 -0300

    Adiciona ao skill_base os multiplicadores de estatísticas base e de combat

--- rpgram/skills/basic_attack.py
+++ rpgram/skills/basic_attack.py
@@ -1,6 +1,7 @@
 
 from rpgram.characters.char_base import BaseCharacter
 from rpgram.enums.skill import SkillTypeEnum, TargetEnum
+from rpgram.enums.stats_combat import CombatStatsEnum
 from rpgram.skills.skill_base import BaseSkill
 
 
@@ -8,68 +9,60 @@ class PhysicalAttack(BaseSkill):
     def __init__(self, char: BaseCharacter):
         name = 'Physical Attack'
         description = 'Ataque Físico baseado em "FOR" e "DES".'
-        power = 0
         level = 1
         cost = 0
 
         super().__init__(
             name=name,
             description=description,
-            power=power,
             level=level,
             cost=cost,
+            base_stats_multiplier={},
+            combat_stats_multiplier={CombatStatsEnum.PHYSICAL_ATTACK: 1.0},
             target_type=TargetEnum.SINGLE,
             skill_type=SkillTypeEnum.ATTACK,
             char=char,
             requirements={},
             damage_types=None
         )
-
-    @property
-    def power(self) -> int:
-        return self.combat_stats.physical_attack
 
 
 class PrecisionAttack(BaseSkill):
     def __init__(self, char: BaseCharacter):
         name = 'Precision Attack'
         description = 'Ataque rápido baseado em "DES".'
-        power = 0
         level = 1
         cost = 0
 
         super().__init__(
             name=name,
             description=description,
-            power=power,
             level=level,
             cost=cost,
+            base_stats_multiplier={},
+            combat_stats_multiplier={CombatStatsEnum.PRECISION_ATTACK: 1.0},
             target_type=TargetEnum.SINGLE,
             skill_type=SkillTypeEnum.ATTACK,
             char=char,
             requirements={},
             damage_types=None
         )
-
-    @property
-    def power(self) -> int:
-        return self.combat_stats.precision_attack
 
 
 class MagicalAttack(BaseSkill):
     def __init__(self, char: BaseCharacter):
         name = 'Magical Attack'
         description = 'Ataque Mágico baseado em "INT" e "WIS".'
-        power = 0
         level = 1
         cost = 0
 
         super().__init__(
             name=name,
             description=description,
-            power=power,
             level=level,
             cost=cost,
+            base_stats_multiplier={},
+            combat_stats_multiplier={CombatStatsEnum.MAGICAL_ATTACK: 1.0},
             target_type=TargetEnum.SINGLE,
             skill_type=SkillTypeEnum.ATTACK,
             char=char,
@@ -77,6 +70,15 @@ class MagicalAttack(BaseSkill):
             damage_types=None
         )
 
-    @property
-    def power(self) -> int:
-        return self.combat_stats.magical_attack
+
+if __name__ == '__main__':
+    from rpgram.constants.test import BASE_CHARACTER
+    pha = PhysicalAttack(BASE_CHARACTER)
+    pra = PrecisionAttack(BASE_CHARACTER)
+    maa = MagicalAttack(BASE_CHARACTER)
+    print(f'Multipliers Text: "{pha.multipliers_text}", Power: {pha.power}')
+    print(f'CHARACTER.Physical Attack: {BASE_CHARACTER.cs.physical_attack}')
+    print(f'Multipliers Text: "{pra.multipliers_text}", Power: {pra.power}')
+    print(f'CHARACTER.Precision Attack: {BASE_CHARACTER.cs.precision_attack}')
+    print(f'Multipliers Text: "{maa.multipliers_text}", Power: {maa.power}')
+    print(f'CHARACTER.Magical Attack: {BASE_CHARACTER.cs.magical_attack}')
