0917c6b4cfa5108148b46f4e7422d0cd005d1af3
Edson Cristovam <pg2006pe@hotmail.com>
2024-06-19 20:51:14 -0300

    Corrige texto de constante

--- bot/constants/skill_tree.py
+++ bot/constants/skill_tree.py
@@ -8,6 +8,6 @@ REFRESH_SKILL_TREE_PATTERN = 'refresh_view_skill_tree'
 
 # ALERT BUTTON TEXTS
 ACCESS_DENIED = (
-    f'⛔VOCÊ NÃO TEM ACESSO A ESSA ÁRVORE DE HOBILIDADES⛔\n\n'
+    f'⛔VOCÊ NÃO TEM ACESSO A ESSA ÁRVORE DE HABILIDADES⛔\n\n'
     f'Use o comando !{COMMANDS[0]} para acessar a SUA ÁRVORE.'
 )
