f5e4b649e01b154c111c097d9105824fe6aab255
Edson Cristovam <pg2006pe@hotmail.com>
2024-07-22 18:15:03 -0300

    Adiciona RaceEnum

---
+++ rpgram/enums/race.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class RaceEnum(Enum):
+    DWARF = 'Anão'
+    ELF = 'Elfo'
+    HALFLING = 'Halfling'
+    HUMAN = 'Humano'
+    ORC = 'Orque'
+    DROW = 'Drow'
+    GOBLIN = 'Goblin'
+    TROLL = 'Troll'
+    KOBOLD = 'Kobold'
+    SPECTRUM = 'Espectro'
+    OGRE = 'Ogro'
+    LYCANTHROPE = 'Licantropo'
+    HARPY = 'Harpia'
+    LAMIA = 'Lâmia'
+    DRACONIAN = 'Draconiano'
+    DRYAD = 'Dríade'
+    SYLPH = 'Sílfide'
+    GNOLL = 'Gnoll'
+    FOMORI = 'Fomori'
+    NEPHILIM = 'Nefilim'
