db85d15088a702ae3a2fa46f5a204499db25354b
Edson Cristovam <pg2006pe@hotmail.com>
2024-07-22 18:16:04 -0300

    Adiciona get_debuffs_emoji_text

--- rpgram/enums/debuff.py
+++ rpgram/enums/debuff.py
@@ -69,12 +69,26 @@ BREAKABLE_IMMOBILIZED_DEBUFFS_NAMES = [
 ]
 
 
-def get_debuff_emoji_text(damage: Union[DebuffEnum, str]) -> str:
-    if isinstance(damage, str):
-        damage = DebuffEnum[damage]
-    name = damage.name
+def get_debuff_emoji_text(debuff_name: Union[DebuffEnum, str]) -> str:
+    '''Retorna string com o emoji e o nome do debuff.
+    '''
+
+    if isinstance(debuff_name, str):
+        debuff_name = DebuffEnum[debuff_name]
+    name = debuff_name.name
 
     return f'{DebuffEmojiEnum[name].value}{name.title()}'
+
+
+def get_debuffs_emoji_text(
+    *debuff_names: Union[DebuffEnum, str],
+    sep: str = ', '
+) -> str:
+    '''Retorna tupla de debuffs como uma string com o emoji e 
+    o nome do debuff separados pelo "sep".
+    '''
+
+    return sep.join([get_debuff_emoji_text(d) for d in debuff_names])
 
 
 if __name__ == '__main__':
