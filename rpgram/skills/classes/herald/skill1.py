babbb7a56d17630ce83706fcd93e8156685a6b2e
Edson Cristovam <pg2006pe@hotmail.com>
2024-08-04 20:09:29 -0300

    Adiciona limitação de listagem geral de skills

--- rpgram/skills/classes/herald/skill1.py
+++ rpgram/skills/classes/herald/skill1.py
@@ -8,7 +8,7 @@ from rpgram.skills.classes.multiclasse.physical_defense import (
 SKILL_WAY_DESCRIPTION = {
     'name': 'Protetor Implacável',
     'description': (
-        'O Protetor Implacável encarna a força indomável da proteção. '
+        'O Protetor Implacável encarna a força indomável para proteger. '
         'Sua devoção à causa da justiça o torna um defensor inflexível '
         'dos fracos e oprimidos. Ele é um escudo inquebrantável, '
         'capaz de absorver o impacto de qualquer ataque e retaliar '
