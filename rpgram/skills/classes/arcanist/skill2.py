7fb8ed752ba1b0a29aeb3d2ef7b1218299e87cd6
Edson Cristovam <pg2006pe@hotmail.com>
2024-10-17 17:53:41 -0300

    Adiciona skill DarkShotSkill

--- rpgram/skills/classes/arcanist/skill2.py
+++ rpgram/skills/classes/arcanist/skill2.py
@@ -1,10 +1,10 @@
 from typing import TYPE_CHECKING
 from rpgram.conditions.debuff import BlindnessCondition
-from rpgram.conditions.target_skill_debuff import MuddyCondition
-from rpgram.constants.text import EVASION_EMOJI_TEXT, MAGICAL_ATTACK_EMOJI_TEXT
+from rpgram.constants.text import MAGICAL_ATTACK_EMOJI_TEXT
 from rpgram.enums.classe import ClasseEnum
 from rpgram.enums.damage import DamageEnum, get_damage_emoji_text
 from rpgram.enums.debuff import DebuffEnum, get_debuff_emoji_text
+from rpgram.enums.emojis import EmojiEnum
 from rpgram.enums.skill import (
     ArcanistSkillEnum,
     SkillDefenseEnum,
@@ -14,17 +14,68 @@ from rpgram.enums.skill import (
 from rpgram.enums.stats_combat import CombatStatsEnum
 from rpgram.requirement import Requirement
 from rpgram.skills.classes.multiclasse.magical_attack import (
-    EarthBreakSkill,
-    FireBallSkill,
-    PrismaticShotSkill,
-    WaterBubbleSkill,
-    WindGustSkill
+    PrismaticShotSkill
 )
 from rpgram.skills.skill_base import BaseSkill
 
 
 if TYPE_CHECKING:
     from rpgram.characters.char_base import BaseCharacter
+
+
+class DarkShotSkill(BaseSkill):
+    NAME = ArcanistSkillEnum.DARK_SHOT.value
+    DESCRIPTION = (
+        f'Canaliza a *Energia Mágica* e dispara um *Feixe Trevoso*, '
+        f'causando dano de '
+        f'*{get_damage_emoji_text(DamageEnum.DARK)}* com base no '
+        f'*{MAGICAL_ATTACK_EMOJI_TEXT}* (100% + 5% x Rank x Nível) e '
+        f'adicionando a condição '
+        f'*{get_debuff_emoji_text(DebuffEnum.BLINDNESS)}* com nível igual ao '
+        f'(Rank x Nível) se for *Acerto Crítico*{EmojiEnum.DICE.value}.'
+    )
+    RANK = 1
+    REQUIREMENTS = Requirement(**{
+        'classe_name': ClasseEnum.ARCANIST.value,
+    })
+
+    def __init__(self, char: 'BaseCharacter', level: int = 1):
+        base_stats_multiplier = {}
+        combat_stats_multiplier = {
+            CombatStatsEnum.MAGICAL_ATTACK: 1.00,
+        }
+        damage_types = [DamageEnum.DARK]
+
+        super().__init__(
+            name=DarkShotSkill.NAME,
+            description=DarkShotSkill.DESCRIPTION,
+            rank=DarkShotSkill.RANK,
+            level=level,
+            base_stats_multiplier=base_stats_multiplier,
+            combat_stats_multiplier=combat_stats_multiplier,
+            target_type=TargetEnum.SINGLE,
+            skill_type=SkillTypeEnum.ATTACK,
+            skill_defense=SkillDefenseEnum.MAGICAL,
+            char=char,
+            use_equips_damage_types=False,
+            requirements=DarkShotSkill.REQUIREMENTS,
+            damage_types=damage_types
+        )
+
+    def hit_function(
+        self,
+        target: 'BaseCharacter',
+        damage: int,
+        total_damage: int,
+    ) -> dict:
+        report = {'text': ''}
+        if target.is_alive and self.dice.is_critical:
+            level = self.level_rank
+            blindness_condition = BlindnessCondition(level=level)
+            status_report = target.status.add_condition(blindness_condition)
+            report['status_text'] = status_report['text']
+
+        return report
 
 
 SKILL_WAY_DESCRIPTION = {
@@ -55,3 +106,13 @@ if __name__ == '__main__':
         verbose=True,
     )['text'])
     ARCANIST_CHARACTER.skill_tree.learn_skill(PrismaticShotSkill)
+
+    skill = DarkShotSkill(ARCANIST_CHARACTER)
+    print(skill)
+    print(ARCANIST_CHARACTER.cs.magical_attack)
+    print(ARCANIST_CHARACTER.to_attack(
+        defender_char=ARCANIST_CHARACTER,
+        attacker_skill=skill,
+        verbose=True,
+    )['text'])
+    ARCANIST_CHARACTER.skill_tree.learn_skill(DarkShotSkill)
