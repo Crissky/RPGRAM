2cd026c9c8fa873fc219a01affdc3b775a689f75
Edson Cristovam <pg2006pe@hotmail.com>
2024-11-12 19:12:23 -0300

    Atualização de mensagem de remoção do status.

--- rpgram/status.py
+++ rpgram/status.py
@@ -380,8 +380,8 @@ class Status:
                     if report['text']:
                         report['text'] += '\n'
                     report['text'] += (
-                        f'Condição "{condition.emoji_name}" '
-                        f'foi removida do Status.'
+                        f'Condição *{condition.emoji_name}* '
+                        f'foi removida.'
                     )
                 reports.append(report)
 
