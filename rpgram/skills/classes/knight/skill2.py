207072f2f8bf737ac2e535bc5386c2fc25dcbe45
Crissky <pg2006pe@hotmail.com>
2025-02-18 18:58:49 -0300

    WIP: Adicionando skill2 de knight

---
+++ rpgram/skills/classes/knight/skill2.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from rpgram.characters.char_base import BaseCharacter
+
+
+SKILL_WAY_DESCRIPTION = {
+    'name': '',
+    'description': (
+        ''
+    ),
+    'skill_list': [
+        
+    ]
+}
+
+
+if __name__ == '__main__':
+    from rpgram.constants.test import KNIGHT_CHARACTER
