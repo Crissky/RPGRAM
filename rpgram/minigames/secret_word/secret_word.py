90c355d3f6a7c3eff52aef77df2f33ef61bbf65c
Crissky <pg2006pe@hotmail.com>
2025-09-13 22:39:30 -0300

    Refatora letter_set para correct_letter_set

--- rpgram/minigames/secret_word/secret_word.py
+++ rpgram/minigames/secret_word/secret_word.py
@@ -35,7 +35,7 @@ class SecretWordGame:
             self.num_try = 0
             self.word_list = []
             self.check_list = []
-            self.letter_set = set()
+            self.correct_letter_set = set()
         else:
             raise TypeError(
                 f'Rarity deve ser do tipo RarityEnum ou String. '
@@ -60,7 +60,7 @@ class SecretWordGame:
             if clean_word[i] == clean_secret_word[i]:
                 check[i] = '🟩'
                 letters[i] = None
-                self.letter_set.add(clean_word[i])
+                self.correct_letter_set.add(clean_word[i])
 
         for i in range(size):
             if check[i] == '🟩':
@@ -68,7 +68,7 @@ class SecretWordGame:
             if clean_word[i] in letters:
                 check[i] = '🟨'
                 letters[letters.index(clean_word[i])] = None
-                self.letter_set.add(clean_word[i])
+                self.correct_letter_set.add(clean_word[i])
 
         check_text = ''.join(check)
         result_secret_word = self.secret_word.upper()
@@ -77,7 +77,7 @@ class SecretWordGame:
         self.word_list.append(result_word)
         check_list = self.check_list.copy()
         word_list = self.word_list.copy()
-        letter_set = self.letter_set.copy()
+        correct_letter_set = self.correct_letter_set.copy()
         is_correct = check == ['🟩'] * size
         if not is_correct:
             self.num_try += 1
@@ -89,6 +89,7 @@ class SecretWordGame:
                 f'`{check}`\n\n'
             )
         text += 'Letras na palavra: '
+        text += ', '.join(sorted(correct_letter_set)).upper()
         text += ', '.join(sorted(letter_set)).upper()
         text += '\n'
         result = {
@@ -99,7 +100,7 @@ class SecretWordGame:
             'text': text,
             'word': result_word,
             'word_list': word_list,
-            'letter_set': letter_set
+            'correct_letter_set': correct_letter_set,
         }
 
         return result
